e28b5514a6e1d7f2665e66f429b2e62104b29b21
lch1204 <93138803+lch1204@users.noreply.github.com>
2024-10-02 20:38:45 +0300

    не могла создаться вся структура, поэтому тут закомментировано большинство и немного переделан парсинг

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <QCoreApplication>
 #include "nmea0183.h"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    NMEA::NMEA0183 tranzas("COM10");
+    NMEA::NMEA0183 tranzas("ttyACM0");
+//    std::cout << "Press Enter to exit..." << std::endl;
+//    std::cin.get();
     return a.exec();
 }
